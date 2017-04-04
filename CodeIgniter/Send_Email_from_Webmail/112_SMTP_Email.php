75ca09e3ef1ac186525ad7e8f53f9e064e78d628
riazul701 <riazul701@gmail.com>
2017-04-04 17:18:13 +0600

    update code

---
+++ CodeIgniter/Send_Email_from_Webmail/112_SMTP_Email.php
@@ -0,0 +1,31 @@
+<?php
+
+public function send_email($email_to, $email_subject, $email_message) {
+        $from_email = 'test@domain-name.com';
+        //configure email settings
+        $config = Array(
+            'mailtype'  => 'html',
+            'protocol' => 'smtp',
+            'smtp_host' => 'mail.domain-name.com',
+            'smtp_port' => 465,
+            'smtp_crypto' => 'ssl',
+            'smtp_user' => 'test@domain-name.com',
+            'smtp_pass' => 'pass1234',
+            'charset'   => 'utf-8',
+            'mailpath' => '/usr/sbin/sendmail',
+            'charset'   => 'iso-8859-1',
+            'newline'   => "\r\n",
+            'wordwrap' => TRUE,
+            'smtp_auth' => TRUE
+        );
+        $this->load->library('email', $config);
+        //$this->email->set_mailtype("html");
+
+        //send mail
+        $this->email->from($from_email,'Company Name');
+        $this->email->to("$email_to");
+        $this->email->subject($email_subject);
+        $this->email->message($email_message);
+
+        $email_result = $this->email->send();
+    }
